0c59251d2dd1656fcabe0fd91baef77808fc8f62
giogio1551 <xz454@drexel.edu>
2021-11-08 03:33:37 -0500

    Timer/Operator/Level Design
    Timer/Operator/Level Design

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] private float time;
+    private Text countdowntime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdowntime = gameObject.GetComponent<Text>();
+        countdowntime.text = time.ToString("F0");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            countdowntime.text = time.ToString("F0");
+        }
+       // else
+       // {
+       //     SceneManager.LoadScene("");
+       // }
+    }
+}
